0d1d10d868102777bf94d07a6e82e58dae02a750
Larissa Pinheiro <rissapins@gmail.com>
2024-12-22 13:52:10 -0300

    adicionando o formsmodel e para receber dados no modo two-way data binding

--- src/app/appointment-list/appoiment-list.component.ts
+++ src/app/appointment-list/appoiment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+  newAppointmentTitle: string = "";
+	newAppointmentDate: Date = new Date();
 
+  appointment: Appointment[] = []
 }
