29b0127fa549a04497529cb1a5555e87c994e327
Larissa Pinheiro <rissapins@gmail.com>
2024-12-22 17:39:02 -0300

    removendo um item a partir do index (splice)

--- src/app/appointment-list/appoiment-list.component.ts
+++ src/app/appointment-list/appoiment-list.component.ts
@@ -10,22 +10,24 @@ export class AppointmentListComponent {
   newAppointmentTitle: string = "";
 	newAppointmentDate: Date = new Date();
 
-  appointment: Appointment[] = []
+  appointments: Appointment[] = []
 
   addAppointment() {
     if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
-      const formattedDate = this.newAppointmentDate.toISOString().split('T')[0];
-
       let newAppointment: Appointment = {
-        id: this.appointment.length + 1,
+        id: this.appointments.length + 1,
         title: this.newAppointmentTitle,
         date: this.newAppointmentDate
       }
 
-      this.appointment.push(newAppointment);
+      this.appointments.push(newAppointment);
 
       this.newAppointmentTitle = "";
 	    this.newAppointmentDate = new Date();
     }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1)
   }
 }
